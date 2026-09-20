08333c80d912fcec433a55116260ba4e452c0113
Ritik Raushan <ritikraushan5941>
2026-09-20 11:05:24 +0530

    Update project

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-
 import Header from "../components/common/Header/Header";
 import Footer from "../components/common/Footer/Footer";
 import Hero from "../components/home/Hero/Hero";
@@ -6,4 +5,17 @@ import ServiceStrip from "../components/home/ServiceStrip/ServiceStrip";
 import AboutPreview from "../components/home/AboutPreview/AboutPreview";
 import WhyChoose from "../components/home/WhyChoose/WhyChoose";
 
-export default function Home(){return <><Header/><main><Hero/><ServiceStrip/><AboutPreview/><WhyChoose/></main><Footer/></>}
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <ServiceStrip />
+        <AboutPreview />
+        <WhyChoose />
+      </main>
+      <Footer />
+    </>
+  );
+}
